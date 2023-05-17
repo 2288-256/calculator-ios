5c419489c41e6354772df28c32ef4d6dcfcd6093
Hiryuto <108036759+Hiryuto@users.noreply.github.com>
2023-05-17 15:40:28 +0900

    v1.0.0 完成

--- dentaku/ViewController.swift
+++ dentaku/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var displayLavel:UILabel!
+    @IBOutlet var flagLavel:UILabel!
     var tempNumber:String = "0"
     var resultNum:Int = 0
-    var digitNum:Int = 1
+    var resultNum2:Int = 0
+    var digitNum:Int = 0
     var flag:String = "null"
-    var countNum:Int = 0
+    var countNum1:Int = 0
+    var countNum2:Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,7 +25,10 @@ class ViewController: UIViewController {
     }
     func addNum(addNumber:Int){
         digitNum += 1
-        if digitNum <= 7{
+        if digitNum == 1{
+            tempNumber = ""
+        }
+        if digitNum <= 6{
             tempNumber = tempNumber + String(addNumber)
             displayLavel.text = String(tempNumber)
         }else{
@@ -33,21 +39,93 @@ class ViewController: UIViewController {
         addNum(addNumber: sender.tag)
     }
     @IBAction func reset(){
+        flagLavel.text = ""
         tempNumber = "0"
         flag = "null"
-        digitNum = 1
+        digitNum = 0
         displayLavel.text = String(tempNumber)
     }
-    @IBAction func result(){
-        switch flag {
-        case "null":
-            alert(titleText: "エラー", messageText: "文字を入れてください")
-        default:
-            print("test")
+    @IBAction func sigh(sender: UIButton){
+        if flag != "null"{
+           return alert(titleText: "エラー", messageText: "デモ版のため計算記号を続けて\n押すことはできません")
         }
-    }
-    @IBAction func plus(){
+        switch sender.tag {
+        case 11:
+            flag = "plus"
+            countNum1 = Int(tempNumber)!
+            tempNumber = "0"
+            displayLavel.text = "0"
+            digitNum = 0
+            flagLavel.text = String(countNum1) + "+"
+        case 12:
+            flag = "subtraction"
+            countNum1 = Int(tempNumber)!
+            tempNumber = "0"
+            displayLavel.text = "0"
+            digitNum = 0
+            flagLavel.text = String(countNum1) + "-"
+        case 13:
+            flag = "multiplication"
+            countNum1 = Int(tempNumber)!
+            tempNumber = "0"
+            displayLavel.text = "0"
+            digitNum = 0
+            flagLavel.text = String(countNum1) + "×"
+        case 14:
+            flag = "division"
+            countNum1 = Int(tempNumber)!
+            tempNumber = "0"
+            displayLavel.text = "0"
+            digitNum = 0
+            flagLavel.text = String(countNum1) + "÷"
+        default:
+            alert(titleText: "エラー", messageText: "予期せぬエラーです")
+        }
         
+    }
+    
+    @IBAction func result(){
+        flagLavel.text = ""
+        switch flag {
+        case "plus":
+            let temp:Int = Int(tempNumber)!
+            resultNum = countNum1 + temp
+            displayLavel.text = String(resultNum)
+            tempNumber = String(resultNum)
+            flag = "null"
+        case "subtraction":
+            let temp:Int = Int(tempNumber)!
+            resultNum = countNum1 - temp
+            displayLavel.text = String(resultNum)
+            tempNumber = String(resultNum)
+            flag = "null"
+        case "multiplication":
+            let temp:Int = Int(tempNumber)!
+            resultNum = countNum1 * temp
+            displayLavel.text = String(resultNum)
+            tempNumber = String(resultNum)
+            flag = "null"
+        case "division":
+            let temp:Int = Int(tempNumber)!
+            if tempNumber == "0"{
+                alert(titleText: "エラー", messageText: "0で割ることはできません")
+                return reset()
+            }
+            resultNum = countNum1 / temp
+            resultNum2 = countNum1 % temp
+            
+            if resultNum2 != 0{
+                displayLavel.text = String(resultNum) + "あまり" + String(resultNum2)
+            }else{
+                displayLavel.text = String(resultNum)
+            }
+            tempNumber = String(resultNum)
+            flag = "null"
+        case "null":
+            alert(titleText: "エラー", messageText: "計算記号を押してください")
+        default:
+            alert(titleText: "エラー", messageText: "予期せぬエラーです")
+        }
     }
     func alert(titleText:String,messageText:String){
         let alert = UIAlertController(title: titleText, message: messageText, preferredStyle: .alert)
